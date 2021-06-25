850571504f0c501b5d842b23fd04f9935e0b1a0c
Daniel Xu <dxu@dxuuu.xyz>
2021-06-25 16:44:01 -0700

    Use OUT_DIR for bpf skeleton

--- below/build.rs
+++ below/build.rs
@@ -1,4 +1,4 @@
-use std::fs::create_dir_all;
+use std::env;
 use std::path::Path;
 
 use libbpf_cargo::SkeletonBuilder;
@@ -6,16 +6,8 @@ use libbpf_cargo::SkeletonBuilder;
 const SRC: &str = "./src/bpf/exitstat.bpf.c";
 
 fn main() {
-    // It's unfortunate we cannot use `OUT_DIR` to store the generated skeleton.
-    // Reasons are because the generated skeleton contains compiler attributes
-    // that cannot be `include!()`ed via macro. And we cannot use the `#[path = "..."]`
-    // trick either because you cannot yet `concat!(env!("OUT_DIR"), "/skel.rs")` inside
-    // the path attribute either (see https://github.com/rust-lang/rust/pull/83366).
-    //
-    // However, there is hope! When the above feature stabilizes we can clean this
-    // all up.
-    create_dir_all("./src/bpf/.output").unwrap();
-    let skel = Path::new("./src/bpf/.output/exitstat.skel.rs");
+    let path = env::var("OUT_DIR").unwrap() + "/exitstat.skel.rs";
+    let skel = Path::new(&path);
     SkeletonBuilder::new(SRC).generate(&skel).unwrap();
     println!("cargo:rerun-if-changed={}", SRC);
 }
--- below/src/open_source/mod.rs
+++ below/src/open_source/mod.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#[path = "../bpf/.output/exitstat.skel.rs"]
+#[path = concat!(env!("OUT_DIR"), "/exitstat.skel.rs")]
 mod bpf;
 pub mod commands;
 pub mod init;
